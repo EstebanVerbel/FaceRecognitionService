2d276f39f0c679d95aa41b252d5e9aa67df47427
EstebanVerbel <verbelesteban2@gmail.com>
2018-09-18 21:05:28 -0400

    Created service to perform comparison

---
+++ services/facecomparisonservice.go
@@ -0,0 +1,10 @@
+package facecomparisonservice
+
+// Compare compares two images of faces and returns a score of the likelihood that it's the same person
+func Compare() {
+
+	// TODO:
+	// * Take two images params to compare
+	// * Implement call to cognitive service api
+	// * return score
+}

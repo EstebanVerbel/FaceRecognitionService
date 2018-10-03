1d8a07380aa183c690013b6659389c3fc7cfe575
EstebanVerbel <verbelesteban2@gmail.com>
2018-10-02 22:42:31 -0400

    Updated face detection service to use []byte body instead of json with url

--- main.go
+++ main.go
@@ -1,10 +1,50 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+
 	"github.com/Dev/FaceRecognitionService/services/facecompserv"
+
+	_ "image/jpeg"
 )
 
 func main() {
+
+	imageFile, err := os.Open("Angelina.jpg")
+
+	if err != nil {
+		// error
+		fmt.Println("Error reading image: ", err)
+	}
+
+	defer imageFile.Close()
+
+	imageFile.Seek(0, 0)
+
+	imageData, imageType, err := image.Decode(imageFile)
+	if err != nil {
+		// Handle error
+		fmt.Println("Error Decoding Image: ", err)
+	}
+
+	fmt.Println(imageType)
+
+	buf := new(bytes.Buffer)
+
+	err = jpeg.Encode(buf, imageData, nil)
+	if err != nil {
+		fmt.Println("Error Encodimg image into byte: ", err)
+	}
+
+	imageByteArray := buf.Bytes()
+
+	facecompserv.Detect(imageByteArray)
+
+	fmt.Println("done")
 
 	// TODO:
 	// Run face detection on first image
@@ -17,7 +57,7 @@ func main() {
 	// const faceID1 string = "8f1f117b-809a-45d3-8b1d-fcc83f88b7d9"
 	// const faceID2 string = "da857626-1ed4-408c-9fc9-378e86147907"
 
-	facecompserv.Detect()
+	// facecompserv.Detect()
 
 	// f = facecompserv.Compare(faceID1, faceID2)
 
--- services/facecompserv/facedetectionservice.go
+++ services/facecompserv/facedetectionservice.go
@@ -3,11 +3,11 @@ package facecompserv
 // API doc: https://westus.dev.cognitive.microsoft.com/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/Dev/FaceRecognitionService/models/constants"
@@ -15,16 +15,22 @@ import (
 )
 
 // Detect faces on an image
-func Detect() faceservice.FaceDetectResponse {
+func Detect(image []byte) faceservice.FaceDetectResponse {
 
 	const imageURL = "http://okmagazine.com/wp-content/uploads/2017/07/Angelina-Jolie-Bells-Palsy-Vanity-Fair-Interview-Long.jpg"
 	//const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Angelina_Jolie_2_June_2014_%28cropped%29.jpg/220px-Angelina_Jolie_2_June_2014_%28cropped%29.jpg"
 
+	// C:\Angelina.jpg
+
 	const uri = constants.FaceDetectURIBase + constants.FaceDetectParams
 
-	const imageURLEnc = "{\"url\":\"" + imageURL + "\"}"
+	// const imageURLEnc = "{\"url\":\"" + imageURL + "\"}"
 
-	reader := strings.NewReader(imageURLEnc)
+	// reader := strings.NewReader(imageURLEnc)
+
+	//reader := strings.NewReader(image)
+
+	reader := bytes.NewBuffer(image)
 
 	// Create the Http client
 	client := &http.Client{
@@ -38,22 +44,26 @@ func Detect() faceservice.FaceDetectResponse {
 	}
 
 	// Add headers
-	req.Header.Add("Content-Type", "application/json")
+	//req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/octet-stream")
 	req.Header.Add("Ocp-Apim-Subscription-Key", constants.SubscriptionKey1)
 
 	// Send the request and retrieve the response
 	resp, err := client.Do(req)
+
 	if err != nil {
-		panic(err)
+		fmt.Println("Error on http request:", err)
 	}
 
 	defer resp.Body.Close()
+
+	fmt.Println("Response Code: ", resp.StatusCode)
 
 	// Read the response body.
 	// Note, data is a byte array
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		fmt.Println("Erro while reading data from response: ", err)
 	}
 
 	var faceResponse faceservice.FaceDetectResponse
@@ -66,7 +76,14 @@ func Detect() faceservice.FaceDetectResponse {
 	}
 
 	if len(detectionResponseArray) == 1 {
+
+		fmt.Println("Face Detected Succesfully")
+
 		faceResponse = detectionResponseArray[0]
+
+		fmt.Println(faceResponse.FaceID)
+		fmt.Println(faceResponse.FaceAttributes.Gender)
+
 		return faceResponse
 	} else if len(detectionResponseArray) > 1 {
 		// error, more than one face on image

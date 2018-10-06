946d630fc868c9854489e41750f2f6c6d39e29d5
EstebanVerbel <verbelesteban2@gmail.com>
2018-10-06 19:59:34 -0400

    Implemented test code and added test image

--- FaceDetectionService/main.go
+++ FaceDetectionService/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+
+	"github.com/Dev/FaceRecognitionService/FaceDetectionService/services"
+)
+
 func main() {
+	imageFile, err := os.Open("Angelina.jpg")
+
+	if err != nil {
+		// error
+		fmt.Println("Error reading image: ", err)
+	}
+
+	defer imageFile.Close()
+
+	imageFile.Seek(0, 0)
+
+	imageData, imageType, err := image.Decode(imageFile)
+	if err != nil {
+		// Handle error
+		fmt.Println("Error Decoding Image: ", err)
+	}
+
+	fmt.Println(imageType)
+
+	buf := new(bytes.Buffer)
+
+	err = jpeg.Encode(buf, imageData, nil)
+	if err != nil {
+		fmt.Println("Error Encodimg image into byte: ", err)
+	}
+
+	imageByteArray := buf.Bytes()
+
+	services.Detect(imageByteArray)
 
 }

32c7adc04dee56c4e1b00baa05b8b79bdbda0f0a
EstebanVerbel <verbelesteban2@gmail.com>
2018-10-06 20:07:30 -0400

    Created face comparison service project and added constants

---
+++ FaceComparisonService/models/constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+// FaceCompURIBase is the base uri for Azure's face verification service
+const FaceCompURIBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0/verify?"
+
+// BynaryContentType contains string to define []byte content type on http requests
+const BynaryContentType = "application/octet-stream"
+
+// JSONContentType contains string to define Json content type on http requests
+const JSONContentType = "application/json"
+
+// ContentTypeHeader string Header
+const ContentTypeHeader = "Content-Type"
+
+// SubscriptionKeyHeader string header
+const SubscriptionKeyHeader = "Ocp-Apim-Subscription-Key"

cdd57f383485c539d15c511ad00cea97790e0eef
EstebanVerbel <verbelesteban2@gmail.com>
2018-10-31 21:41:15 -0400

    Added user registration method

---
+++ FaceValidationService/helpers/dbhelper/userhelper.go
@@ -0,0 +1,23 @@
+package dbhelper
+
+import (
+	"log"
+
+	"github.com/gocql/gocql"
+)
+
+// RegisterUser registers users to application (insert into db)
+func RegisterUser(email string, lastname string, firstName string) {
+
+	session := GetSession()
+
+	// Todo:
+	// make sure user does not already exist
+
+	var version = gocql.TimeUUID()
+
+	if err := session.Query(`INSERT INTO users (email, last_name, name, version) VALUES (?,?,?,?)`,
+		email, lastname, firstName, version).Exec(); err != nil {
+		log.Fatal(err)
+	}
+}

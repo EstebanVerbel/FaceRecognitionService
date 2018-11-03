20fa6a13cf10c0664f71243bcc21af5fc9915b69
EstebanVerbel <verbelesteban2@gmail.com>
2018-11-03 16:54:20 -0400

    Updated registration method to include validation for when user already exists

--- FaceValidationService/helpers/dbhelper/userhelper.go
+++ FaceValidationService/helpers/dbhelper/userhelper.go
@@ -7,22 +7,50 @@ import (
 )
 
 // RegisterUser registers users to application (insert into db)
-func RegisterUser(email string, lastname string, firstName string) {
+func RegisterUser(username *string, lastname *string, firstName *string) {
+
+	var isUserExistsAlready = verifyIfUserExists(username)
+
+	if isUserExistsAlready {
+		// TODO Esteban: Return error message or something when user already exists
+		// log it to error logs
+		return
+	}
 
 	session := GetSession()
 
-	// Todo:
-	// make sure user does not already exist
-
 	var version = gocql.TimeUUID()
 
-	if err := session.Query(`INSERT INTO users (email, last_name, name, version) VALUES (?,?,?,?)`,
-		email, lastname, firstName, version).Exec(); err != nil {
+	if err := session.Query(`INSERT INTO users (username, last_name, name, version) VALUES (?,?,?,?)`,
+		*username, *lastname, *firstName, version).
+		Exec(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// verifyIfUserExists checks if username exists in database
+func verifyIfUserExists(username *string) bool {
+
+	session := GetSession()
+	var existingUsername string
+
+	if err := session.Query(`SELECT "username" FROM users WHERE "username" = ?`, &username).
+		Consistency(gocql.Quorum).
+		Scan(&existingUsername); err != nil {
+		log.Fatal(err)
+	}
+
+	// if username exists in database
+	if len(existingUsername) > 0 {
+		return true
+	}
+
+	return false
 }
 
 // Todo: Implement following
 
 // Get User Images
 // Add user images
+// Add User Image
+// Set face id for Image
